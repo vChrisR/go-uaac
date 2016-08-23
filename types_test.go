1164c3190ae7d7eb9ad28ecd6e2be04aea00c4ac
David Malone <dmalone@pivotal.io>
2016-08-23 16:18:56 -0500

    fixing paths for testdata directory in tests

--- types_test.go
+++ types_test.go
@@ -190,10 +190,10 @@ func TestJSONUnmarshallIdentityZone(t *testing.T) {
 }
 
 func TestJSONUnmarshallUsersWithUaa20Model(t *testing.T) {
-	responseBody, err := ioutil.ReadFile("../testdata/uaa-list-users-2.0.json")
+	responseBody, err := ioutil.ReadFile("./testdata/uaa-list-users-2.0.json")
 
 	if err != nil {
-		panic("Failed to read ../testdata/uaa-list-users-2.0.json: " + err.Error())
+		panic("Failed to read ./testdata/uaa-list-users-2.0.json: " + err.Error())
 	}
 
 	var users Users
